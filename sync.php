930a348b4c3d6833b6533614d601301701de61db
daobv <dao.hunter@gmail.com>
2014-11-25 04:22:37 +0700

    update sync.php file

--- sync.php
+++ sync.php
@@ -1,19 +1,27 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
 $connect = mysql_connect("127.0.0.1","root","12345") or die(" khong the ket noi");
 mysql_select_db("smart_ad");
 mysql_query('SET NAMES utf8');
 $query=mysql_query("SELECT * FROM s_users");
-//$myfile = fopen("user.txt", "w") or die("Unable to open file!");
 $i= 1;
+$insertSql = "INSERT INTO(username,app_name,click,action,date) VALUES ";
 while($row=mysql_fetch_array($query))
 {
-    $txt = $row["uname"].",".$row['upassword'];
-    if(isset($row['email'])){
-        $txt .= ",".$row['email'];
+    $url = "http://smartad.vn/report?username=".$row['username'];
+    $data = file_get_contents($url);
+    $contents = json_decode($data);
+    $counter = count($contents);
+    foreach($contents as $key => $content){
+        $username = $row['username'];
+        $appName = $content['app_name'];
+        $click = $content['click'];
+        $action = $content['action'];
+        $time = date('ymd',strtotime($content['date']));
+        $insertSql .= " ('".$username."','".$appName."','".$click."','".$action."','".$time."') ";
+        if($key < $contents){
+            $insertSql .= ", ";
+        }
     }
-    echo $i." ".$row["uname"]."<br/>";
-    $i++;
 }
-
+$query=mysql_query($insertSql);
 
